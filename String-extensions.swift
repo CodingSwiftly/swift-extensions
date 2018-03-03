4c08d5c9c052f1f0f998354d43728eca58024b27
Kaushik <medcharla@users.noreply.github.com>
2018-03-02 21:35:50 -0800

    Fine tuning
    Removed redundant variables from CharacterSet

--- String-extensions.swift
+++ String-extensions.swift
@@ -1,45 +1,45 @@
-extension CharacterSet {
-  static let spaceCharSet: CharacterSet = {
-    return CharacterSet(charactersIn: String.singleSpace)
-  }()
-  
-  static let spaceTabCharSet: CharacterSet = {
-    return CharacterSet(charactersIn: String.singleSpace + String.tabSpace)
-  }()
-  
-  static let spaceTabNewLineCharSet: CharacterSet = {
-    return CharacterSet(charactersIn: String.singleSpace + String.tabSpace + String.newLine)
-  }()
-}
-
 extension String {
   static let emptyString = ""
   static let singleSpace = " "
-  static let tabSpace = "\t"
   static let newLine = "\n"
   
+  var isValid: Bool {
+    return (self != nil && self != String.emptyString)
+  }
+  
+  var singleLineWords:[String]? { // returns array of trimmed words/strings
+    guard !contains(String.newLine) else { return nil }
+    return components(separatedBy: CharacterSet.whitespaces).filter{$0.isValid}
+  }
+  
+  var multiLineWords:[String] { // returns array of trimmed 'lines'
+    if contains(String.newLine) {
+      return components(separatedBy: CharacterSet.newlines).filter{$0.isValid}.map{$0.trimmedString() ?? ""}
+    }
+    return components(separatedBy: CharacterSet.whitespacesAndNewlines).filter{$0.isValid}
+  }
+  
+  var words:[String] { // returns array of trimmed words/strings. Joins lines
+    return components(separatedBy: CharacterSet.whitespacesAndNewlines).filter{$0.isValid}
+  }
+  
+  func trimmedString() -> String? {
+    guard let snglLineWrds = singleLineWords else { return nil }
+    return snglLineWrds.joined(separator: String.singleSpace)
+  }
+  
   mutating func trimSingleLine() {
-    self = words.joined(separator: String.singleSpace)
+    if let trimmedString = trimmedString() {
+      self = trimmedString
+    }
   }
 
+  mutating func trimMultiLines() {
+    self = multiLineWords.joined(separator: String.newLine)
+  }
+  
   mutating func trimParagraph() {
     self = words.joined(separator: String.singleSpace)
-  }
-  
-  mutating func trimMultiLines() {
-    trimParagraph()
-  }
-  
-  var singleLineWords:[String] {
-    return components(separatedBy: CharacterSet.spaceTabCharSet).filter{$0 != String.emptyString}
-  }
-  
-  var multiLineWords:[String] {
-    return components(separatedBy: CharacterSet.spaceTabNewLineCharSet).filter{$0 != String.emptyString}
-  }
-  
-  var words:[String] {
-    return multiLineWords
   }
   
   func toCharacter() -> Character? {
